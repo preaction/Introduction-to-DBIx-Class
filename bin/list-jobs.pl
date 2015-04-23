3196f5820c7bd35271495def2821e75a85dd5592
Doug Bell <doug.bell@baml.com>
2015-04-23 17:08:25 -0500

    move latest_run to Result class
    Putting helper methods on Result and ResultSet objects are better than
    cpoying them all over.

--- bin/list-jobs.pl
+++ bin/list-jobs.pl
@@ -8,14 +8,7 @@ use My::Schema;
 my $schema = My::Schema->connect( 'dbi:SQLite:data.db' );
 my @jobs = $schema->resultset( 'Job' )->all;
 for my $job ( @jobs ) {
-    my $latest_run_rs = $job->runs->search(
-        { },
-        {
-            order_by => { -desc => 'start' },
-            limit => 1,
-        },
-    );
-    if ( my $run = $latest_run_rs->next ) {
+    if ( my $run = $job->latest_run ) {
         say sprintf '%s (%s): %s (%s)',
             $job->name, $job->type,
             $run->status, $run->start;
--- lib/My/Schema/Result/Job.pm
+++ lib/My/Schema/Result/Job.pm
@@ -6,4 +6,19 @@ __PACKAGE__->set_primary_key( 'id' );
 __PACKAGE__->has_many(
     runs => 'My::Schema::Result::Run' => 'job_id',
 );
+
+sub latest_run {
+    my ( $self ) = @_;
+
+    my $latest_run_rs = $self->runs->search(
+        { },
+        {
+            order_by => { -desc => 'start' },
+            limit => 1,
+        },
+    );
+
+    return $latest_run_rs->next;
+}
+
 1;
