2c3c2c41c1141f93a08be0a0f19e3b99d9c93acb
Doug Bell <doug.bell@baml.com>
2015-04-23 14:40:34 -0500

    add run information to job list

--- bin/list-jobs.pl
+++ bin/list-jobs.pl
@@ -8,5 +8,20 @@ use My::Schema;
 my $schema = My::Schema->connect( 'dbi:SQLite:data.db' );
 my @jobs = $schema->resultset( 'Job' )->all;
 for my $job ( @jobs ) {
-    say join ": ", $job->type, $job->name;
+    my $latest_run_rs = $job->runs->search(
+        { },
+        {
+            order_by => { -desc => 'start' },
+            limit => 1,
+        },
+    );
+    if ( my $run = $latest_run_rs->next ) {
+        say sprintf '%s (%s): %s (%s)',
+            $job->name, $job->type,
+            $run->status, $run->start;
+    }
+    else {
+        say sprintf '%s (%s): Never run',
+            $job->name, $job->type;
+    }
 }
